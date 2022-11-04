c9c20ca8bf8dd11562c6cd52f97a701c6f88149e
Genoveva Villablanca <gvillablancav.externo@bancofalabella.cl>
2022-11-03 21:06:02 -0300

    nueva linea de prueba

--- src/westbank/WestBank.java
+++ src/westbank/WestBank.java
@@ -8,6 +8,7 @@ public class WestBank {
 
     public static void main(String[] args) {
         System.out.println("holitos soy una nueva linea :3");
+        System.out.println("holitos soy una nueva linea nuevamente jejejejeje ");
     }
     
 }
