c96ebf548ef1bf2257b98509d69bef493412d49f
Genoveva Villablanca <gvillablancav.externo@bancofalabella.cl>
2022-11-03 23:09:34 -0300

    Menu

--- src/westbank/WestBank.java
+++ src/westbank/WestBank.java
@@ -6,7 +6,31 @@ import clases.Cuenta;
 public class WestBank {
 
     public static void main(String[] args) {
-        
+        System.out.println("Bienvenidos, por favor seleccione una opcion para continuar...");
+        System.out.println("1- Registrar Cliente\n2- Ver Datos de Cliente \n3- Depositar \n4- Girar \n5- Consultar Saldo \n6- Salir");
+        String opcion = new String();
+        while(opcion.isEmpty()){
+            System.out.println("Ingrese codigo del producto: ");
+            Scanner op = new Scanner(System.in);
+            opcion = op.nextLine();
+            
+            
+            if(opcion.isEmpty()){
+                System.out.println("Favor seleccionar una opcion");
+            }
+        }
     }
     
+    public static String registrarCliente(){
+        return "";
+    }
+    
+    
+     public static String verDatos(){
+        return "";
+    }
+     
+     public static String salida(){
+        return "";
+    }
 }
