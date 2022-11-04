c55e7a7cfee3e32bdff0378ad1d8fbaf056bfd22
Genoveva Villablanca <gvillablancav.externo@bancofalabella.cl>
2022-11-04 19:24:28 -0300

    funcion verDatos

--- src/westbank/WestBank.java
+++ src/westbank/WestBank.java
@@ -13,16 +13,16 @@ public class WestBank {
     }
     
     public static void menu(List<Cliente> clienteBanco){
-        System.out.println("Bienvenidos, por favor seleccione una opcion para continuar...");
-        System.out.println("1- Registrar Cliente\n2- Ver Datos de Cliente \n3- Depositar \n4- Girar \n5- Consultar Saldo \n6- Salir");
+        System.out.println("BIENVENIDO, PARA CONTINUAR SELECCIONE UNA DE LAS SIGUIENTES OPCIONES...\n");
+        System.out.println("1- REGISTRAR CLIENTE \n2- VER DATOS DE CLIENTES \n3- DEPOSITAR \n4- GIRAR \n5- CONSULTAR SALDO \n6- SALIR");
         String opcion = new String();
         while(opcion.isEmpty()){
-            System.out.println("opcion: ");
+            System.out.println("OPCION: ");
             Scanner op = new Scanner(System.in);
             opcion = (op.nextLine()).trim();
             
             if(opcion.isEmpty()){
-                System.out.println("Favor seleccionar una opcion");
+                System.out.println("FAVOR SELECCIONAR UNA OPCION");
             }
             else if(opcion.equals("1")){
                 registrarCliente(clienteBanco);
@@ -89,13 +89,13 @@ public class WestBank {
             telefono = telefonoIn.nextLine();
             
             if(rut.isEmpty() || nombre.isEmpty() || apellidoPaterno.isEmpty() || apellidoMaterno.isEmpty() || domicilio.isEmpty() || comuna.isEmpty() || telefono.isEmpty()){
-                System.out.println("Favor Ingresar todos los datos\n");
+                System.out.println("FAVOR INGRESAR TODOS LOS DATOS....\n");
             }
             else{
                 for(int i = 0;i<=clienteBanco.size();i++){
                     if(clienteBanco.size()>0){
                         if(clienteBanco.get(i).getRut().equals(rut)){
-                            System.out.println("Cliente ya se registra en sistema");                            
+                            System.out.println("CLIENTE YA SE REGISTRA EN SISTEMA....\n");                            
                             break;
                         }
                         else{
@@ -111,7 +111,7 @@ public class WestBank {
                             cuenta.setNumeroCuenta(Integer.parseInt(numeroCuenta));
                             cuenta.setSaldo(saldo);                
                             clienteBanco.add(cliente);
-                            System.out.println("Cliente registrado exitosamente!");
+                            System.out.println("CLIENTE REGISTRADO EXITOSAMENTE!\n");
                             break;
                          }                     
                     }
@@ -130,22 +130,52 @@ public class WestBank {
                         cuenta.setNumeroCuenta(Integer.parseInt(numeroCuenta));
                         cuenta.setSaldo(saldo);                
                         clienteBanco.add(cliente);
-                        System.out.println("Cliente registrado exitosamente!");
+                        System.out.println("CLIENTE REGISTRADO EXITOSAMENTE!\n");
                         break;
                     }
                 }                     
             }
         }
-        
-        System.out.println("opcion 1");
         menu(clienteBanco);
         return "1";
     }    
     
     public static String verDatos(List<Cliente> clienteBanco){
-            System.out.println("opcion 2");
-            menu(clienteBanco);
-            return "2";
+        String rut = new String();        
+        while(rut.isEmpty()){
+            System.out.println("RUT: ");
+            Scanner rutIn = new Scanner(System.in);
+            rut = rutIn.nextLine();
+            
+            if(rut.isEmpty()){
+                System.out.println("FAVOR INGRESAR RUT PARA MOSTRAR DATOS...\n");
+            }
+            else{
+                for(int i = 0;i<=clienteBanco.size();i++){
+                    if(clienteBanco.size()>0){
+                        if(clienteBanco.get(i).getRut().equals(rut)){
+                                    System.out.println("DATOS CLIENTES:");
+                                    System.out.println("*********************************************************************");
+                                    System.out.println( "RUT: " + clienteBanco.get(i).getRut()+
+                                                        "\nNOMBRE: " + clienteBanco.get(i).getNombre()+
+                                                        "\nAPELLIDO PATERNO: " + clienteBanco.get(i).getApellidoPaterno()+
+                                                        "\nAPELLIDO MATERNO: " + clienteBanco.get(i).getApellidoMaterno()+
+                                                        "\nDOMICILIO: " + clienteBanco.get(i).getDomicilio()+
+                                                        "\nCOMUNA: " + clienteBanco.get(i).getComuna()+
+                                                        "\nNUMERO DE CUENTA: " + clienteBanco.get(i).getCuenta().getNumeroCuenta()+
+                                                        "\nSALDO: " + clienteBanco.get(i).getCuenta().getSaldo());
+                            break;
+                        }
+                        else{
+                            System.out.println("CLIENTE NO SE REGISTRA EN SISTEMA...");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        menu(clienteBanco);
+        return "2";
     }
     
     public static String deposito(List<Cliente> clienteBanco){
@@ -190,7 +220,7 @@ public class WestBank {
     }
      
     public static String salida(){
-        System.out.println("opcion 6");
+        System.out.println("HASTA PRONTO...");
         return "6";
     }
 }
